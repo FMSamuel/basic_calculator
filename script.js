677967fbaa9a2822e17197c119188ad5f0dced7b
FMSamuel <jc007rifle@yahoo.com>
2026-01-22 19:47:23 +0000

    Adding calculation operations to the jc file.

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ function getNumbers() {
     const number2 = parseFloat(document.getElementById('num2').value);
     return { number1, number2 };
 }
+
+function calculate(operation) {
+    const { numb1, numb2 } = getNumbers();
+    let result;
+    switch (operation) {
+        case "addition":
+            result = numb1 + numb2;
+            break;
+        case "subtraction":
+            result = numb1 - numb2;
+            break;
+        case "multiplication":
+            result = numb1 * numb2;
+            break;
+        case "division":
+            if (numb1 >= numb2)
+                result = numb1 / numb2;
+            else
+                result = numb2 / numb1;
+            break;
+        default:
+            result = "Invalid operation";
+    }
+
+    return result;
+}
