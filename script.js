2ef558437aa45f39b79e54cc0e4417ebaa4a46b5
FMSamuel <jc007rifle@yahoo.com>
2026-01-29 14:41:51 +0000

    Add comments to the tester lines.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getNumbers() {
     const number1 = parseFloat(document.getElementById('num1').value);
     const number2 = parseFloat(document.getElementById('num2').value);
 
-    document.getElementById('addition').addEventListener('click', console.log(number1, number2));
+    //document.getElementById('addition').addEventListener('click', console.log(number1, number2));
 
     return { number1, number2 };
 }
